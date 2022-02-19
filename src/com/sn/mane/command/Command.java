9540012be112522a5ee444446f0a5117d1934e24
Alioune MANE <aliounemane@finetechgroupe.com>
2022-02-19 23:29:57 +0000

    Design Pattern Command

---
+++ src/com/sn/mane/command/Command.java
@@ -0,0 +1,12 @@
+package com.sn.mane.command;
+
+/**
+ *
+ * Command interface, declares an interface for executing an operation
+ *
+ */
+public interface Command {
+
+    void execute();
+
+}
---
+++ src/com/sn/mane/command/ConcreteCommand.java
@@ -0,0 +1,21 @@
+package com.sn.mane.command;
+
+
+/**
+ *
+ * ConcreteCommand class, defines a binding between a Receiver object and an
+ * operation
+ *
+ */
+public class ConcreteCommand implements Command {
+
+    private Receiver receiver;
+
+    public ConcreteCommand(Receiver receiver) {
+        this.receiver = receiver;
+    }
+
+    public void execute() {
+        this.receiver.action();
+    }
+}
